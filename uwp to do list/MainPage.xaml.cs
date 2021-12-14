b837dfca5006db2f5d9ba22b94cf5f3029106ffe
KelvinJPS <kelvinnjps10@gmail.com>
2021-12-14 00:46:51 -0500

    create class task in view_model

--- uwp to do list/MainPage.xaml.cs
+++ uwp to do list/MainPage.xaml.cs
@@ -50,21 +50,7 @@ namespace uwp_to_do_list
 
         }
 
-        public class Task : INotifyPropertyChanged
-        {
-            public string Name_task { get; set; }
-            public DateTime Reminder { get; set; }
-            public string name_list { get; set; }
-            public DateTime date { get; set; }
-            public string subtask { get; set; }
-            public string priority { get; set; }
-
-            public event PropertyChangedEventHandler PropertyChanged;
-            private void NotifyPropertyChanged(string propertyName)
-            {
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
+     
 
 
         public static ObservableCollection<Task> GetTask(string connectionString)
---
+++ uwp to do list/view_model/task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Task : INotifyPropertyChanged
+{
+    public string Name_task { get; set; }
+    public DateTime Reminder { get; set; }
+    public string name_list { get; set; }
+    public DateTime date { get; set; }
+    public string subtask { get; set; }
+    public string priority { get; set; }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    private void NotifyPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
